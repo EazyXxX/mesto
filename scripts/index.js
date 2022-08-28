96fc4d8c4c3b028d4d0ba6c55b00de6ef7af8852
EazyXxX <104682962+EazyXxX@users.noreply.github.com>
2022-08-28 11:26:41 +0300

    v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,85 @@
-let popup = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup__form')
-let openButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__cross-button');
-let saveButton = popup.querySelector('.popup__save-button');
-let profileName = document.querySelector('.profile__name');
-let profileSubname = document.querySelector('.profile__description');
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupCard = document.querySelector('.popup_type_card');
+const editPopupForm = document.querySelector('.popup__form_type_edit');
+const cardPopupForm = document.querySelector('.popup__form_type_card');
+const openButtonEdit = document.querySelector('.profile__edit-button');
+const openButtonCard = document.querySelector('.profile__plus-button');
+const closeButtonEdit = document.querySelector('.popup__cross-button_type_edit');
+const closeButtonCard = document.querySelector('.popup__cross-button_type_card');
+const saveButton = popupEdit.querySelector('.popup__save-button');
+const profileName = document.querySelector('.profile__name');
+const profileSubname = document.querySelector('.profile__description');
 
-const openPopup = function () {
-    popup.classList.add('popup_opened');
+const openPopupEdit = function () {
+    popupEdit.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileSubname.textContent;
 }
 
-const closePopup = function () {
-    popup.classList.remove('popup_opened');
+const closePopupEdit = function () {
+    popupEdit.classList.remove('popup_opened');
 }
 
-openButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+const openPopupCard = function () {
+    popupCard.classList.add('popup_opened');
+}
 
-let nameInput = popup.querySelector('.popup__input_type_name');
-let jobInput = popup.querySelector('.popup__input_type_subname');
+const closePopupCard = function () {
+    popupCard.classList.remove('popup_opened');
+}
 
-function formSubmitHandler(evt) {
+openButtonEdit.addEventListener('click', openPopupEdit);
+closeButtonEdit.addEventListener('click', closePopupEdit);
+openButtonCard.addEventListener('click', openPopupCard);
+closeButtonCard.addEventListener('click', closePopupCard)
+
+let nameInput = popupEdit.querySelector('.popup__input_type_name');
+let jobInput = popupEdit.querySelector('.popup__input_type_subname');
+
+function editFormSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileSubname.textContent = jobInput.value;
-    closePopup();
+    closePopupEdit();
 }
 
-popupForm.addEventListener('submit', formSubmitHandler);
+editPopupForm.addEventListener('submit', editFormSubmitHandler);
+
+
+const elementsList = document.querySelector('#list');
+
+
+function cardFormSubmitHandler(evt) {
+    evt.preventDefault();
+    let cardInput = document.querySelector('.popup__input_type_card-name').value;
+let linkInput = document.querySelector('.popup__input_type_link').value;
+
+const html = `
+<li class="elements__card">
+<img class="elements__image" src="${linkInput}" alt="${cardInput}">
+<div class="elements__card-footer">
+    <h2 class="elements__name">${cardInput}</h2>
+    <button class="elements__like" type="button" aria-label="лайк">
+    </button>
+</div>
+</li>
+`
+    elementsList.insertAdjacentHTML('afterbegin', html);
+    closePopupCard();
+}
+
+cardPopupForm.addEventListener('submit', cardFormSubmitHandler);
+
+const like = document.querySelectorAll('.elements__like');
+
+console.log(like)
+
+const getLike = function() {
+    like.forEach(like.classList.toggle("elements__like_active"));
+    like.addEventListener("click", getLike);
+}
+
+console.log(getLike)
+
+
+
