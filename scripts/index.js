237e25b4caa2414548d4b62e1229d6b91b90f157
EazyXxX <104682962+EazyXxX@users.noreply.github.com>
2022-09-04 19:58:39 +0300

    v2

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
 const popupEdit = document.querySelector('.popup_type_edit');
 const popupCard = document.querySelector('.popup_type_card');
+const popupPicture = document.querySelector('.popup_type_picture');
 const editPopupForm = document.querySelector('.popup__form_type_edit');
 const cardPopupForm = document.querySelector('.popup__form_type_card');
 const openButtonEdit = document.querySelector('.profile__edit-button');
 const openButtonCard = document.querySelector('.profile__plus-button');
 const closeButtonEdit = document.querySelector('.popup__cross-button_type_edit');
 const closeButtonCard = document.querySelector('.popup__cross-button_type_card');
+const closeButtonPicture = document.querySelector('.popup__cross-button_type_picture');
 const saveButton = popupEdit.querySelector('.popup__save-button');
 const profileName = document.querySelector('.profile__name');
 const profileSubname = document.querySelector('.profile__description');
@@ -28,10 +30,19 @@ const closePopupCard = function () {
     popupCard.classList.remove('popup_opened');
 }
 
+const openPopupPicture = function () {
+    popupPicture.classList.add('popup_opened');
+}
+
+const closePopupPicture = function () {
+    popupPicture.classList.remove('popup_opened');
+}
+
 openButtonEdit.addEventListener('click', openPopupEdit);
 closeButtonEdit.addEventListener('click', closePopupEdit);
 openButtonCard.addEventListener('click', openPopupCard);
 closeButtonCard.addEventListener('click', closePopupCard)
+closeButtonPicture.addEventListener('click', closePopupPicture);
 
 let nameInput = popupEdit.querySelector('.popup__input_type_name');
 let jobInput = popupEdit.querySelector('.popup__input_type_subname');
@@ -58,7 +69,7 @@ function addCard(e) {
     const cardInput = document.querySelector('.popup__input_type_card-name').value;
     newCardElement.querySelector('.elements__name').textContent = cardInput;
     newCardElement.querySelector('.elements__image').alt = cardInput;
-    newCardElement.querySelector('.elements__image').src = linkInput;
+    newCardElement.querySelector('.elements__image').style = `background-image: url(${linkInput})`;
 
     //deletion
     newCardElement.querySelector('.elements__delete-button').addEventListener("click", (e) => {
@@ -67,6 +78,15 @@ function addCard(e) {
     }
     );
 
+    //bigPicture    
+    newCardElement.querySelector('.elements__image').addEventListener("click", openPopupPicture);
+    const newPhoto = function () {
+        document.querySelector('.popup__picture').src = `${linkInput}`;
+        document.querySelector('.popup__pic-moniker').textContent = `${cardInput}`;
+    };
+    newCardElement.querySelector('.elements__image').addEventListener("click", newPhoto);
+
+    //creating new card
     elementsList.prepend(newCardElement);
     closePopupCard();
 
@@ -104,34 +124,84 @@ glebCard.querySelector('.elements__delete-button').addEventListener("click", (e)
     elementItem.remove();
 }
 );
+glebCard.querySelector('.elements__image').style = `background-image: url(../../../images/Gleb.png)`;
+glebCard.querySelector('.elements__image').addEventListener('click', openPopupPicture);
+const glebInfo = function () {
+    document.querySelector('.popup__picture').src = `./images/Gleb.png`;
+    document.querySelector('.popup__pic-moniker').textContent = 'Глебос';
+}
+glebCard.querySelector('.elements__image').addEventListener("click", glebInfo);
+
 
 ilyaCard.querySelector('.elements__delete-button').addEventListener("click", (e) => {
     const elementItem = e.target.closest('.elements__card');
     elementItem.remove();
 }
 );
+ilyaCard.querySelector('.elements__image').style = `background-image: url(../../../images/Ilya.png)`;
+ilyaCard.querySelector('.elements__image').addEventListener('click', openPopupPicture);
+const ilyaInfo = function () {
+    document.querySelector('.popup__picture').src = `./images/Ilya.png`;
+    document.querySelector('.popup__pic-moniker').textContent = 'Илья';
+}
+ilyaCard.querySelector('.elements__image').addEventListener("click", ilyaInfo);
+
 
 germanCard.querySelector('.elements__delete-button').addEventListener("click", (e) => {
     const elementItem = e.target.closest('.elements__card');
     elementItem.remove();
 }
 );
+germanCard.querySelector('.elements__image').style = `background-image: url(../../../images/German.png)`;
+germanCard.querySelector('.elements__image').addEventListener('click', openPopupPicture);
+const germanInfo = function () {
+    document.querySelector('.popup__picture').src = `./images/German.png`;
+    document.querySelector('.popup__pic-moniker').textContent = 'Герман';
+}
+germanCard.querySelector('.elements__image').addEventListener("click", germanInfo);
+
 
 jurezCard.querySelector('.elements__delete-button').addEventListener("click", (e) => {
     const elementItem = e.target.closest('.elements__card');
     elementItem.remove();
 }
 );
+jurezCard.querySelector('.elements__image').style = `background-image: url(../../../images/Jurez.png)`;
+jurezCard.querySelector('.elements__image').addEventListener('click', openPopupPicture);
+const jurezInfo = function () {
+    document.querySelector('.popup__picture').src = `./images/Jurez.png`;
+    document.querySelector('.popup__pic-moniker').textContent = 'Юрец';
+}
+jurezCard.querySelector('.elements__image').addEventListener("click", jurezInfo);
+
+
 
 pashaCard.querySelector('.elements__delete-button').addEventListener("click", (e) => {
     const elementItem = e.target.closest('.elements__card');
     elementItem.remove();
 }
 );
+pashaCard.querySelector('.elements__image').style = `background-image: url(../../../images/Pasha.png)`;
+pashaCard.querySelector('.elements__image').addEventListener('click', openPopupPicture);
+const pashaInfo = function () {
+    document.querySelector('.popup__picture').src = `./images/Pasha.png`;
+    document.querySelector('.popup__pic-moniker').textContent = 'Паша';
+}
+pashaCard.querySelector('.elements__image').addEventListener("click", pashaInfo);
+
+
 
 mishaCard.querySelector('.elements__delete-button').addEventListener("click", (e) => {
     const elementItem = e.target.closest('.elements__card');
     elementItem.remove();
 }
 );
+mishaCard.querySelector('.elements__image').style = `background-image: url(../../../images/Misha.png)`;
+mishaCard.querySelector('.elements__image').addEventListener('click', openPopupPicture);
+const mishaInfo = function () {
+    document.querySelector('.popup__picture').src = `./images/Misha.png`;
+    document.querySelector('.popup__pic-moniker').textContent = 'Мишган';
+}
+mishaCard.querySelector('.elements__image').addEventListener("click", mishaInfo);
+
 
